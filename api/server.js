aeff6f07002c01f8ac28484ccdb95e3a31383703
KareemHassan <kareem99710@gmail.com>
2025-07-02 19:10:29 +0300

    change port

--- api/server.js
+++ api/server.js
@@ -40,6 +40,7 @@ app.post('/api/send-email', async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log('✅ Server running at http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
